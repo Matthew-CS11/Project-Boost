31ca6156dccb6a58d3a30ae907c2f572cafa4640
Matthew-CS11 <2260213@flourbluffschools.org>
2026-01-12 12:31:07 -0600

    added moving obsticle

--- project-boost/player/player.gd
+++ project-boost/player/player.gd
@@ -4,33 +4,38 @@ class_name Player
 @export_range(750, 2500) var force := 1000.0
 @export var torque_thrust := 100.0
 
+var transitioning := false
+
 func _process(delta: float) -> void:
-	
-	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * force)
-		
-	if Input.is_action_pressed("rotate_left"):
-		apply_torque(Vector3(0, 0, delta*torque_thrust))
-		
-	if Input.is_action_pressed("rotate_right"):
-		apply_torque(Vector3(0, 0, -delta*torque_thrust))
+	if !transitioning:
+		if Input.is_action_pressed("boost"):
+			apply_central_force(basis.y * delta * force)
+			
+		if Input.is_action_pressed("rotate_left"):
+			apply_torque(Vector3(0, 0, delta*torque_thrust))
+			
+		if Input.is_action_pressed("rotate_right"):
+			apply_torque(Vector3(0, 0, -delta*torque_thrust))
 
 
 func _on_body_entered(body: Node) -> void:
-	if "Goal" in body.get_groups():
-		print("Win")
-		if body.file_path:
-			complete_level(body.file_path)
-		else:
-			print("no level")
-	if "Hazard" in body.get_groups():
-		crash_sequence()
+	if !transitioning:
+		if "Goal" in body.get_groups():
+			print("Win")
+			if body.file_path:
+				complete_level(body.file_path)
+			else:
+				print("no level")
+		if "Hazard" in body.get_groups():
+			crash_sequence()
 		
 func crash_sequence() -> void:
 	print("kaboom")
+	transitioning = true
 	await get_tree().create_timer(2).timeout
 	get_tree().reload_current_scene.call_deferred()
 
 func complete_level(next_level_file) -> void:
+	transitioning = true
 	await get_tree().create_timer(2).timeout
 	get_tree().change_scene_to_file.call_deferred(next_level_file)
