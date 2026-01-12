b0de39d13da1637e5de9ec25426718b15f3c1ef5
Matthew-CS11 <2260213@flourbluffschools.org>
2026-01-12 12:41:00 -0600

    added sound

--- project-boost/player/player.gd
+++ project-boost/player/player.gd
@@ -4,13 +4,20 @@ class_name Player
 @export_range(750, 2500) var force := 1000.0
 @export var torque_thrust := 100.0
 
+@onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
+@onready var success_audio: AudioStreamPlayer = $SuccessAudio
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+
 var transitioning := false
 
 func _process(delta: float) -> void:
 	if !transitioning:
 		if Input.is_action_pressed("boost"):
 			apply_central_force(basis.y * delta * force)
-			
+			if !rocket_audio.is_playing():
+				rocket_audio.play()
+		else:
+			rocket_audio.stop()
 		if Input.is_action_pressed("rotate_left"):
 			apply_torque(Vector3(0, 0, delta*torque_thrust))
 			
@@ -31,11 +38,13 @@ func _on_body_entered(body: Node) -> void:
 		
 func crash_sequence() -> void:
 	print("kaboom")
+	explosion_audio.play()
 	transitioning = true
 	await get_tree().create_timer(2).timeout
 	get_tree().reload_current_scene.call_deferred()
 
 func complete_level(next_level_file) -> void:
 	transitioning = true
+	success_audio.play()
 	await get_tree().create_timer(2).timeout
 	get_tree().change_scene_to_file.call_deferred(next_level_file)
